1576fecf909914f1a2913a23ebac8c5d736b2d41
JJSliderman <jorkhan4@gmail.com>
2026-02-08 17:38:42 -0500

    Check

--- Spring-Boot-Application/src/main/java/com/example/MyApplication.java
+++ Spring-Boot-Application/src/main/java/com/example/MyApplication.java
@@ -3,7 +3,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
-// Tester
 
 @RestController
 @SpringBootApplication
