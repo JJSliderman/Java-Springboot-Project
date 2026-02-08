55fedd89c5b1f6293c5f96aa4a82164e5e85d863
Justin Hazrati <jhazrati@ipaglobal.com>
2026-02-08 17:25:25 -0500

    Gitignore changes

--- Spring-Boot-Application/src/main/java/com/example/MyApplication.java
+++ Spring-Boot-Application/src/main/java/com/example/MyApplication.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 
-// Stuff
-
 @RestController
 @SpringBootApplication
 public class MyApplication {
